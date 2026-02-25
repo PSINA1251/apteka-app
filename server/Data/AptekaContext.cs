96e5be8d4d35b63f5d13048779b6aa316311ed3d
PSINA1251 <90103237+PSINA1251@users.noreply.github.com>
2026-02-25 21:09:29 +0600

    Add files via upload

---
+++ server/Data/AptekaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AptekaAPI.Models;
+
+namespace AptekaAPI.Data
+{
+    public class AptekaContext : DbContext
+    {
+        public AptekaContext(DbContextOptions<AptekaContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Лекарство> Лекарства { get; set; }
+        public DbSet<Заказ> Заказы { get; set; }
+        public DbSet<СоставЗаказа> СоставыЗаказов { get; set; }
+    }
+}
